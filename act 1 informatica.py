ba91bbe44f2d4b022bbad31b3ca68c00c5dc55c2
losnazis <pelamanzo123@gmail.com>
2025-03-12 14:13:34 -0300

    Add files via upload

---
+++ act 1 informatica.py
@@ -0,0 +1,14 @@
+numeros=[1,2,30,4,51]
+for numero in numeros:
+    cuadrado=numero**2
+    print(f "el cuadrado de{numero} es {caudrado}")
+
+print(numeros[4])
+
+#tratamiento de cadenas
+nombre="marcos"
+apellido="mendoza"
+frace="hola esta es una frase"
+
+longitud=len(frace)
+print(longitud)
